e8555ca601068d68c764e5413b2f25bcb88a575a
akash-kobal-ics4u1-01-25 <akash.kobal@student.tdsb.on.ca>
2025-10-19 22:04:39 -0400

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -7,14 +7,15 @@ public class PracticeProblem {
 public static int find(int[] array, int number) {
 	for (int i = 0; i < array.length; i++) {
 		if (array[i] == number) {
-		return i;
+			return i;
+		}
 	}
-}
 	return -1;
 }
+	
 //Question 2
 public static int findLast(String[] array, String string) {
-	for (int i = (array.length - 1); i >= 0; i--) {
+	for (int i = array.length - 1; i >= 0; i--) {
 		if (array[i] == string) {
 			return i;
 		}
@@ -24,19 +25,23 @@ public static int findLast(String[] array, String string) {
 //Question 3
 public static int findSecond(char[] array, char character) {
 	int count = 0;
+	int firstIndex = 0;
 	for (int i = 0; i < array.length; i++) {
-		if (array[i] == character) {
-			count++;
-			if (count == 2) {
-				return i;
-			}
+		if (array[i] == character)
+		count+= 1;
+		if (count == 1) {
+		firstIndex = i;
 		}
+		if (count == 2) {
+		return i;
+	}
 	}
 	if (count == 1) {
-		for (int i = 0; i < array.length; i++) {
-			if (array[i] == character) return i;
-		}
+		return firstIndex;
 	}
 	return -1;
 }
 }
+
+
+
